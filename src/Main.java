18e5265f3b387eda4128ba12cb884ecd224acc0d
vzavolsky <vzavolsky@ya.ru>
2023-03-26 00:38:30 +0300

    Course 2 - Homework 001 Память в Java. Zavolsky V.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Объявите объект класса person с полями name и surname.
+        Переопределите у него метод toString так, чтобы он возвращал строку из name и surname.
+        Объявите метод changePerson, который принимает параметр типа Person под названием person.
+        Внутри метода с помощью оператора присваивания запишите в параметр новый объект типа Person.
+        Присвойте строке name значение Ilya, строке surname — значение Lagutenko.
+        В методе main объявите переменную типа Person под названием person и запишите новый объект типа Person.
+        Присвойте строке name значение Lyapis, строке surname значение — Trubetskoy.
+        В следующей строке вызовите метод changePerson и передайте в него эту переменную.*/
+
+    }
+
+}
